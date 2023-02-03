a8263f1798fb6c0e62650c050d1dbb1ae0bb09ec
Heather Gibling <heathergibling@gmail.com>
2023-02-03 14:00:30 -0500

    add batch of scripts

---
+++ code/chapter4/sim-graph-alignments/different-aligners/align-ga-allele-msa-nopath-100-0.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -cwd
+#$ -t 1-36
+a=$(awk "NR==$SGE_TASK_ID" /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/names.txt)
+cd /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/graphs
+module load GraphAligner
+for i in $(seq 1 100); do
+GraphAligner -g allele-msa-nopath-chop1024.gfa -f /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/simulations/READS/haploid/100/reads/$a-flank10k-100-bp-250-frag-100-x-e-0-i-$i.r1.fq -f /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/simulations/READS/haploid/100/reads/$a-flank10k-100-bp-250-frag-100-x-e-0-i-$i.r2.fq -x vg -a /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/TEMP-$a-allele-msa-nopath-100-0.gaf
+sed 's/dv:f://' /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/TEMP-$a-allele-msa-nopath-100-0.gaf | awk -v a="$a" -v i="$i" -v OFS="," '{print a, "allele-msa", "nopath", "100", "0", i, $1, $14}' >> /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/$a-allele-msa-nopath-100-0-ga-deviations.csv
+rm /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/TEMP-$a-allele-msa-nopath-100-0.gaf
+echo "finished $i"
+done
---
+++ code/chapter4/sim-graph-alignments/different-aligners/align-mg-allele-msa-nopath-100-0.01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#$ -cwd
+#$ -t 1-36
+a=$(awk "NR==$SGE_TASK_ID" /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/names.txt)
+cd /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/graphs
+module load minigraph
+for i in $(seq 1 100); do
+minigraph -x sr allele-msa-nopath-chop1024.gfa /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/simulations/READS/haploid/100/reads/$a-flank10k-100-bp-250-frag-100-x-e-0.01-i-$i.r1.fq /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/simulations/READS/haploid/100/reads/$a-flank10k-100-bp-250-frag-100-x-e-0.01-i-$i.r2.fq | sed 's/dv:f://' | awk -v a="$a" -v i="$i" -v OFS="," '{print a, "allele-msa", "nopath", "100", "0.01", i, $1, $17}' >> /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/$a-allele-msa-nopath-100-0.01-mg-deviations.csv
+echo "finished $i"
+done
