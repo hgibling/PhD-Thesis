aaffad54c75c23e1ac2c546122bd46ce699d159c
Heather Gibling <heathergibling@gmail.com>
2023-02-03 14:01:24 -0500

    add batch of scripts

---
+++ code/chapter4/sim-graph-alignments/different-aligners/align-mg-hg19-path-100-0.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#$ -cwd
+#$ -t 1-36
+a=$(awk "NR==$SGE_TASK_ID" /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/names.txt)
+cd /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/graphs
+module load minigraph
+for i in $(seq 1 100); do
+minigraph -x sr hg19-path-chop1024.gfa /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/simulations/READS/haploid/100/reads/$a-flank10k-100-bp-250-frag-100-x-e-0-i-$i.r1.fq /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/simulations/READS/haploid/100/reads/$a-flank10k-100-bp-250-frag-100-x-e-0-i-$i.r2.fq | sed 's/dv:f://' | awk -v a="$a" -v i="$i" -v OFS="," '{print a, "hg19", "path", "100", "0", i, $1, $17}' >> /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/$a-hg19-path-100-0-mg-deviations.csv
+echo "finished $i"
+done
---
+++ code/chapter4/sim-graph-alignments/different-aligners/align-vg-frag-fixed-stacked-path-100-0.001.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -cwd
+#$ -t 1-36
+a=$(awk "NR==$SGE_TASK_ID" /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/names.txt)
+cd /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/graphs
+module load vg
+for i in $(seq 1 100); do
+vg map -d stacked-path-chop1024 -f /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/simulations/READS/haploid/100/reads/$a-flank10k-100-bp-250-frag-100-x-e-0.001-i-$i.r1.fq -f /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/simulations/READS/haploid/100/reads/$a-flank10k-100-bp-250-frag-100-x-e-0.001-i-$i.r2.fq -I 5000:250:50:0:1 -U -% | sed 's/dv:f://' | awk -v a="$a" -v i="$i" -v OFS="," '{print a, "stacked", "path", "100", "0.001", i, $1, $16}' >> /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/$a-stacked-path-100-0.001-vg-frag-fixed-deviations.csv
+if [ $? -ne 0 ]; then fail "failed on $i"; fi
+echo "finished $i"
+done
