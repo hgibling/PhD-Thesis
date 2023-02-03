65e26ff0168366ad64304a85ea6a48ef055d8060
Heather Gibling <heathergibling@gmail.com>
2023-02-03 14:01:54 -0500

    add batch of scripts

---
+++ code/chapter4/sim-graph-alignments/different-aligners/align-ga-znf-loop-nopath-100-0.01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -cwd
+#$ -t 1-36
+a=$(awk "NR==$SGE_TASK_ID" /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/names.txt)
+cd /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/graphs
+module load GraphAligner
+for i in $(seq 1 100); do
+GraphAligner -g znf-loop-nopath-chop1024.gfa -f /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/simulations/READS/haploid/100/reads/$a-flank10k-100-bp-250-frag-100-x-e-0.01-i-$i.r1.fq -f /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/simulations/READS/haploid/100/reads/$a-flank10k-100-bp-250-frag-100-x-e-0.01-i-$i.r2.fq -x vg -a /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/TEMP-$a-znf-loop-nopath-100-0.01.gaf
+sed 's/dv:f://' /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/TEMP-$a-znf-loop-nopath-100-0.01.gaf | awk -v a="$a" -v i="$i" -v OFS="," '{print a, "znf-loop", "nopath", "100", "0.01", i, $1, $14}' >> /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/$a-znf-loop-nopath-100-0.01-ga-deviations.csv
+rm /.mounts/labs/awadallalab/private/hgibling/PRDM9-Project/HMM/alignments/TEMP-$a-znf-loop-nopath-100-0.01.gaf
+echo "finished $i"
+done
